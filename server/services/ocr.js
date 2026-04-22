b0db2855a2d876fd599afacbfb6bb5aff393f043
Hugh Lawson-Tancred <hugh@tancred.co.uk>
2026-04-22 12:02:55 +0100

    More region fussing

--- server/services/ocr.js
+++ server/services/ocr.js
@@ -170,7 +170,9 @@ async function extractChunk(pdfBuffer, startPage, endPage, totalPages) {
 async function extractTextWithOCR(s3Key) {
   const { S3Client, GetObjectCommand } = require('@aws-sdk/client-s3');
   const s3Client = new S3Client({
-    region: process.env.AWS_REGION,
+    region: 'us-east-1',
+    endpoint: process.env.AWS_ENDPOINT_URL,
+    forcePathStyle: true,
     credentials: {
       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
